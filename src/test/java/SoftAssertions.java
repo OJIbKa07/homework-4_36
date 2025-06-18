35219a2524eac34eaff60cbd041394659d45c5d5
Olga Palushina <lokipypzemli@mail.ru>
2025-06-18 15:05:35 +0300

    changed SoftAssertions.java

--- src/test/java/SoftAssertions.java
+++ src/test/java/SoftAssertions.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,51 +14,39 @@ public class SoftAssertions {
 
     @BeforeAll
     static void basicSettingBrowder() {
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void SoftAssertionsCheck() {
-       /* open("/selenide/selenide");
-        $$(".UnderlineNav-item span").findBy(text("Wiki")).click();
-        $(".markdown-body").shouldHave(text("Soft assertions"));
-        $("div.markdown-body").$("a[href*='SoftAssertions']")
-                .shouldBe(visible).click();
-
-        $("body")
-                .shouldHave(text("JUnit5"))
-                .shouldHave(text("@ExtendWith"))
-                .shouldHave(text("@Test"));
-
-         */
-        /* open("https://github.com/");
-        $x("(.//*[normalize-space(text()) and normalize-space(.)='Pricing'])[1]/following::span[1]").click();
-        $("#query-builder-test").setValue("selenide").pressEnter();
-        $x("(.//*[normalize-space(text()) and normalize-space(.)='Filter'])[1]/following::em[1]").click();
-        $("#wiki-tab").click();
-        $(".markdown-body").shouldHave(text("Soft assertions"));
-        $("div.markdown-body").$("a[href*='SoftAssertions']")
-                .shouldBe(visible).click();
-
-        $("#wiki-wrapper")
-                .shouldHave(text("JUnit5"))
-                .shouldHave(text("@ExtendWith"))
-                .shouldHave(text("@Test"));
-
-
-         */
-
 
         open("https://github.com/");
         // ввести в поле поиска selenide и нажать enter
         $(".header-search-button").click();
         $("#query-builder-test").setValue("selenide").pressEnter();
-        // кликнуть на первый репозиторий из списка найденых
 
-        $$("ul.repo-list li").first().$("a").click();
+        // кликнуть на первый репозиторий из списка найденых
+        $("span.search-match").click();
+
         // проверка: заголовок selenide/selenide
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
+
+        // переход в раздел Wiki
+        $("#wiki-tab").click();
+
+        // проверка что в списке страниц есть страница SoftAssertions
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+
+        // переход на страницу Soft assertions
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+
+        // проверка что внутри есть заголовок JUnit5 и пример кода
+        $("#wiki-wrapper").shouldHave(text("JUnit5"));
+
+        $(withText("Using JUnit5")).scrollTo();
+        $("#wiki-wrapper").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+
 
     }
 
